f27b35a6a938f4d6f4f7bd8accbbe48ddee4b469
Alexandr <mylo-box-27@yandex.ru>
2020-11-17 12:58:32 +0300

    fix href on footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Footer.module.css"
+
+export const NativeFooter = () => {
+    return <div className={s.headerWrapper}>
+        <a href='https://pokeapi.co/'>
+            <div>PokeAPI.co</div>
+        </a>
+    </div>
+}
